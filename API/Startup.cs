e587fd7f3d96e7b150e8232e6ea74c46e6da8dfe
DianaG <dianagaona@pursuit.org>
2019-10-29 14:17:24 -0400

    Persisting data from backend with Axios

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,7 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy => 
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3001");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
